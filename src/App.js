fa30d12fdf3a6b8bf135c94a394dc509d82453cc
leomsfil <leonardo.msfil@gmail.com>
2022-05-16 10:57:56 -0300

    list commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Form from './components/form'
+import Header from './components/header'
+import List from './components/list';
+import React, { useState } from 'react';
 function App() {
+
+  const [todos, setTodos] = useState([{ id: 0, todo: "This is a simple task" }])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Header />
+
+
+      <Form setTodo={setTodos} len={todos.length} />
+
+
+      <List todoList={todos} setTodo={setTodos} />
     </div>
   );
 }
---
+++ src/components/form.js
@@ -0,0 +1,23 @@
+import {React} from "react";
+
+const Form = ({setTodo, len}) => {
+    var itemtoAdd = ""
+    const formHandling = (e)=>{
+        e.preventDefault() 
+        itemtoAdd = e.target.firstChild.value
+        if(itemtoAdd !== ""){
+            setTodo(state => [...state, {id:len,todo:itemtoAdd}])
+        }
+        e.target.firstChild.value = ""
+    }
+    return(
+        <div className="Form_container">
+            <form onSubmit={formHandling}>
+                <input type="text" placeholder="Write a Task"/>
+                <button type="submit">Add</button>
+            </form>
+        </div>
+    )
+}
+
+export default Form
